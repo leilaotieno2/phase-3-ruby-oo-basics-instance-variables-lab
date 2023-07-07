b3a1f7d3edd0a49d695b6ea4f4b6b9ea90cbcbbe
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-07-07 11:45:00 +0300

    first commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+  def name=(name)
+    @this_dogs_name = name
+  end
 
+  def name
+    @this_dogs_name
+  end
+
+  def bark
+    puts "Woof!"
+  end
+  
+  def sit
+    puts "The Dog is sitting"
+  end
 end
